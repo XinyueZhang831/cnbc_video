a3440e47fd42323be286eb855922edbfa5206416
Xinyue Zhang (Amber) <43156916+XinyueZhang831@users.noreply.github.com>
2020-01-18 04:56:05 -0800

    Update CNBC_youtubdl.py

--- CNBC_youtubdl.py
+++ CNBC_youtubdl.py
@@ -10,14 +10,13 @@ import subprocess
 def youtube_pre(path):
     df = pd.read_csv(path)
     for i,r in df.iterrows():
-        if r['video_id'] == 7000109489:
-            try:
-                video_id = r['video_id']
-                part_file = "http://video.cnbc.com/gallery/?video="
-                files = part_file+str(video_id)
-                subprocess.call('youtube-dl ' + files +' --verbose', shell=True)
-            except:
-                pass
+        try:
+            video_id = r['video_id']
+            part_file = "http://video.cnbc.com/gallery/?video="
+            files = part_file+str(video_id)
+            subprocess.call('youtube-dl ' + files +' --verbose', shell=True)
+        except:
+            pass
 
 
 path = ''
